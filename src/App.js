415b689c5393df544ae6271e5d1d2477d4093197
peter6055 <59694246+peter6055@users.noreply.github.com>
2022-10-31 13:36:49 +1100

    task d done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import './App.css';
 import 'antd/dist/antd.css';
-import {Button, Card, Input, Layout, Radio} from "antd";
+import {Button, Card, Input, Layout, Radio, Table} from "antd";
 import {UserOutlined, MailOutlined, EditOutlined} from '@ant-design/icons';
-import React, {useState} from 'react';
-
+import React, {useReducer, useState} from 'react';
+import {initialState, reducer} from "./data/skill.reducer.js";
 
 const {Header, Content, Footer} = Layout;
 
 
 function App() {
+    // ------------------------------------------------------------------- skill ------------------------
+    // skill name field tracking
+    const [skill, setSkill] = useState("");
+    const skillOnChange = (e) => {
+        setSkill(e.target.value);
+    };
 
+    // skill year field tracking
+    const [year, setYear] = useState(null);
+    const yearOnChange = (e) => {
+        setYear(e.target.value);
+    };
+
+    // reducer controller
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const handleAddSkill = async (event) => {
+        event.preventDefault();
+
+        // call reducer
+        dispatch({type: 'add', skill: skill, year: year})
+
+        // when finish clear skill and
+        setSkill("");
+        setYear(null);
+    }
+
+    const handleRemoveSkill = async (event) => {
+        event.preventDefault();
+
+        const id = event.target.closest("button").value;
+
+        // call reducer
+        dispatch({type: 'remove', id: id})
+    }
+
+    // skill table
+    const columns = [
+        {
+            title: 'Skill Name',
+            dataIndex: 'skill',
+            key: 'skill',
+        },
+        {
+            title: 'Year of Experience',
+            dataIndex: 'year',
+            key: 'year',
+            render: (text) => <span>{text} year(s)</span>,
+        },
+        {
+            title: 'Action',
+            dataIndex: 'id',
+            key: 'id',
+            render: (text) => <Button type="primary" value={text} onClick={handleRemoveSkill}>Remove</Button>,
+        }
+    ];
+
+    // ----------------------------------------------------------------- proficiency selection ----------------------
     const [proficiency, setProficiency] = useState(0);
     const proficiencyOnChange = (e) => {
         console.log('radio checked', e.target.value);
         setProficiency(e.target.value);
     };
 
+    // ----------------------------------------------------------------- form submission ----------------------
     const handleSubmit = async (event) => {
         event.preventDefault();
-
+        console.log(state);
     }
+
 
     return (
         <Layout className="layout">
@@ -57,12 +116,20 @@ function App() {
                             <label>Skill</label><br/>
                             <div className={"skill-collection"} style={{"marginTop": "10px"}}>
                                 <Input placeholder="Name" style={{"marginRight": "20px"}}
-                                       prefix={<EditOutlined/>}/>
+                                       prefix={<EditOutlined/>} value={skill} onChange={skillOnChange}/>
                                 <Input placeholder="Experience in Years" style={{"marginRight": "20px"}}
-                                       prefix={<EditOutlined/>}/>
+                                       prefix={<EditOutlined/>} value={year} onChange={yearOnChange}/>
                             </div>
-                            <Button type="dashed" style={{"marginTop": "10px"}}>Add skill</Button>
-                            <br/><br/><br/>
+                            <Button
+                                type="dashed"
+                                style={{"marginTop": "10px"}}
+                                onClick={handleAddSkill}
+                            >
+                                Add skill
+                            </Button>
+                            <br/><br/>
+                            <Table columns={columns} dataSource={state.skillList}/>
+                            <br/><br/>
 
 
                             <label>Proficiency</label><br/>
@@ -78,7 +145,7 @@ function App() {
 
 
                         <div className={"ant-card-footer-inner"}>
-                            <Button type="primary" htmlType="submit">Submit</Button>
+                            <Button type="primary" htmlType="submit" onClick={handleSubmit}>Submit</Button>
                         </div>
 
                     </Card>
---
+++ src/data/skill.reducer.js
@@ -0,0 +1,38 @@
+export const initialState = {
+    counter: 0,
+    skillList: []
+};
+
+export const reducer = (state, action) => {
+    console.log()
+    switch (action.type) {
+        case "add": {
+            const newSkill = {
+                id: state.counter,
+                skill: action.skill,
+                year: action.year,
+            };
+            return {
+                counter: state.counter + 1,
+                skillList: [...state.skillList, newSkill]
+            };
+        }
+        case "remove": {
+
+            // find matching index
+            const idx = state.skillList.findIndex(element => parseInt(element.id) === parseInt(action.id));
+            // console.log(idx)
+
+            const skillLists = Object.assign([], state.skillList);
+            skillLists.splice(idx, 1)
+
+            return {
+                counter: state.counter,
+                skillList: skillLists
+            };
+        }
+        default:
+            return state;
+    }
+
+};
