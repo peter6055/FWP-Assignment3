4c0de71cad4bd385d90c32880a3aef1256028121
peter6055 <59694246+peter6055@users.noreply.github.com>
2022-10-31 16:29:11 +1100

    task c done

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Navbar
 https://ant.design/components/layout/
+
+Name regex
+https://regex101.com/r/zL7vZ3/1
+
+Email regex
+https://regex101.com/r/pO0jF8/1
--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 import 'antd/dist/antd.css';
-import {Button, Card, Input, Layout, Radio, Table} from "antd";
+import {Alert, Button, Card, Input, Layout, Radio, Table} from "antd";
 import {UserOutlined, MailOutlined, EditOutlined} from '@ant-design/icons';
-import React, {useReducer, useState} from 'react';
+import React, {useEffect, useReducer, useState} from 'react';
 import {initialState, reducer} from "./data/skill.reducer.js";
 
 const {Header, Content, Footer} = Layout;
 
 
 function App() {
-    // ------------------------------------------------------------------- skill ------------------------
+    // ------------------------------------------------------------------- Form input tracking ------------------------
+    // fullName field tracking
+    const [fullname, setFullname] = useState("");
+    const fullnameOnChange = (e) => {
+        setFullname(e.target.value);
+    };
+
+    // email field tracking
+    const [email, setEmail] = useState("");
+    const emailOnChange = (e) => {
+        setEmail(e.target.value);
+    };
+
     // skill name field tracking
     const [skill, setSkill] = useState("");
     const skillOnChange = (e) => {
@@ -22,6 +34,132 @@ function App() {
         setYear(e.target.value);
     };
 
+    // proficiency field tracking
+    const [proficiency, setProficiency] = useState("");
+    const proficiencyOnChange = (e) => {
+        setProficiency(e.target.value);
+    };
+
+    // status tracking
+    const [nameStatus, setNameStatus] = useState("");
+    const nameStatusToClass = (nameStatus ? "has-error" : "")
+
+    const [emailStatus, setEmailStatus] = useState("");
+    const emailStatusToClass = (emailStatus ? "has-error" : "");
+
+    // error message for name and year separately
+    const [skillNameStatus, setSkillNameStatus] = useState("");
+    const [skillYearStatus, setSkillYearStatus] = useState("");
+
+    // error light up for name and year separately
+    const skillNameStatusToClass = (skillNameStatus ? "has-error" : "");
+    const skillYearStatusToClass = (skillYearStatus ? "has-error" : "");
+
+    // release add button
+    const [addBtnStatus, setAddBtnStatus] = useState(true);
+    // count release button useEffect run, to stop it running at first time
+    // set to zero once add clicked
+    const [releaseBtnUseEffectCount, setReleaseBtnUseEffectCount] = useState(0);
+
+    // check when to release add button
+    useEffect(() => {
+        // don't run when no input action (first time)
+        if (releaseBtnUseEffectCount > 0) {
+            // clear all validation regards skill when entering
+            setSkillNameStatus("");
+            setSkillYearStatus("");
+            setSkillStatus("");
+
+            // use to solve no callback useState issues of setSkillNameStatus, setSkillYearStatus, and setSkillStatus
+            let hasError = false;
+
+            // data validation
+            if (skill === "" || skill.length > 20) {
+                setSkillNameStatus("Please fill in the skill (Should be less then 20 characters)");
+                setSkillStatus("error");
+                hasError = true;
+            }
+
+            if (year === null || year > 20 || year < 1 || year % 1 !== 0) {
+                setSkillYearStatus("Year of experience field should be 1-20 (whole number)");
+                setSkillStatus("error");
+                hasError = true;
+            }
+
+            // there are value inside input and with no error
+            if (skill != "" && year != "" && hasError === false) {
+                setAddBtnStatus(false);
+            } else {
+                setAddBtnStatus(true);
+            }
+        }
+        // start from the second round
+        setReleaseBtnUseEffectCount(releaseBtnUseEffectCount + 1);
+    }, [skill, year]);
+
+
+    // error light up for skill collection label
+    const [skillStatus, setSkillStatus] = useState("");
+    const skillStatusToClass = (skillStatus ? "has-error" : "")
+
+    const [proficiencyStatus, setProficiencyStatus] = useState("");
+    const proficiencyStatusToClass = (proficiencyStatus ? "has-error" : "")
+
+
+    // ----------------------------------------------------------------- submission  ----------------------
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        console.log(state);
+
+        // clear error check and error message first
+        setNameStatus("");
+        setEmailStatus("");
+        setProficiencyStatus("");
+
+
+        // start validation
+        // ------- name regex check (format and not empty) -------
+        const fullnameRegex = new RegExp("^[a-zA-Z]{2,50}\\s[a-zA-Z]{2,50}(-[a-zA-Z]{2,50})?$");
+        if (!fullnameRegex.test(fullname)) {
+            setNameStatus("Please fill in your full name with the format (Firstname Lastname)");
+
+        } else if (fullname.length > 60) {
+            setNameStatus("Name shouldn't have more than 60 characters");
+
+        }
+
+        // ------- email -------
+        // regex check (format and not empty)
+        const emailRegex = new RegExp("^[a-z0-9]+(?!.*(?:\\+{2,}|\\-{2,}|\\.{2,}))(?:[\\.+\\-]{0,1}[a-z0-9])*@gmail\\.com$");
+        if (!emailRegex.test(email)) {
+            setEmailStatus("Please fill in your email with format (firstname.lastname@gmail.com)");
+        }
+
+        // check email fit firstname.lastname (toLowerCase ensure case won't impact the result)
+        const splitName = fullname.toLowerCase().split(" "); // fullname: cut it to firstname, lastname
+        const splitEmail = email.toLowerCase().split("@"); // email: cut it to firstname.lastname, gmail.com
+        const splitEmailToName = splitEmail[0].split(".") // email: then cut it to firstname, lastname
+        if (!(splitName[0] === splitEmailToName[0] && splitName[1] === splitEmailToName[1])) {
+            setEmailStatus("Please fill in your email with format (firstname.lastname@gmail.com)");
+        }
+
+
+        // ------- proficiency should be one of the option -------
+        if (!(proficiency === "beginner" || proficiency === "intermediate" || proficiency === "advanced")) {
+            setProficiencyStatus("Please select proficiency");
+        }
+
+
+        // ------- at least one skill -------
+        if (state.skillList.length === 0) {
+            setSkillStatus("Please fill in at least one skill");
+        }
+
+    }
+
+
+    // ------------------------------------------------------------------- skill ------------------------
     // reducer controller
     const [state, dispatch] = useReducer(reducer, initialState);
 
@@ -31,9 +169,11 @@ function App() {
         // call reducer
         dispatch({type: 'add', skill: skill, year: year})
 
-        // when finish clear skill and
+        // clear input
         setSkill("");
         setYear(null);
+        setReleaseBtnUseEffectCount(0);
+
     }
 
     const handleRemoveSkill = async (event) => {
@@ -62,22 +202,9 @@ function App() {
             title: 'Action',
             dataIndex: 'id',
             key: 'id',
-            render: (text) => <Button type="primary" value={text} onClick={handleRemoveSkill}>Remove</Button>,
+            render: (text) => <Button danger value={text} onClick={handleRemoveSkill}>Remove</Button>,
         }
     ];
-
-    // ----------------------------------------------------------------- proficiency selection ----------------------
-    const [proficiency, setProficiency] = useState(0);
-    const proficiencyOnChange = (e) => {
-        console.log('radio checked', e.target.value);
-        setProficiency(e.target.value);
-    };
-
-    // ----------------------------------------------------------------- form submission ----------------------
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        console.log(state);
-    }
 
 
     return (
@@ -102,45 +229,81 @@ function App() {
                         size="small"
                     >
                         <div className={"ant-card-body-inner"}>
-                            <label>Full Name</label>
-                            <Input placeholder="Full Name (Firstname Lastname)" style={{"marginTop": "10px"}}
-                                   prefix={<UserOutlined/>}/>
-                            <br/><br/><br/>
-
-
-                            <label>Email</label>
-                            <Input placeholder="Email" style={{"marginTop": "10px"}} prefix={<MailOutlined/>}/>
-                            <br/><br/><br/>
-
-
-                            <label>Skill</label><br/>
-                            <div className={"skill-collection"} style={{"marginTop": "10px"}}>
-                                <Input placeholder="Name" style={{"marginRight": "20px"}}
-                                       prefix={<EditOutlined/>} value={skill} onChange={skillOnChange}/>
-                                <Input placeholder="Experience in Years" style={{"marginRight": "20px"}}
-                                       prefix={<EditOutlined/>} value={year} onChange={yearOnChange}/>
+                            <div className={nameStatusToClass}>
+                                <label>Full Name</label>
+                                <Input placeholder="Full Name (Firstname Lastname)"
+                                       style={{"marginTop": "10px"}}
+                                       prefix={<UserOutlined/>}
+                                       onChange={fullnameOnChange}/>
+                                <br/>
+                                {nameStatus !== "" && <Alert message={nameStatus} type="error" showIcon/>}
+                                <br/><br/>
                             </div>
-                            <Button
-                                type="dashed"
-                                style={{"marginTop": "10px"}}
-                                onClick={handleAddSkill}
-                            >
-                                Add skill
-                            </Button>
-                            <br/><br/>
-                            <Table columns={columns} dataSource={state.skillList}/>
-                            <br/><br/>
+
+                            <div className={emailStatusToClass}>
+                                <label>Email</label>
+                                <Input placeholder="Email" style={{"marginTop": "10px"}}
+                                       prefix={<MailOutlined/>}
+                                       onChange={emailOnChange}/>
+                                <br/>
+                                {emailStatus !== "" && <Alert message={emailStatus} type="error" showIcon/>}
+                                <br/><br/>
+                            </div>
 
 
-                            <label>Proficiency</label><br/>
-                            <Radio.Group onChange={proficiencyOnChange} value={proficiency}
-                                         className={"proficiency-collection"}
-                                         style={{"marginTop": "10px"}}>
-                                <Radio value={"beginner"}>Beginner</Radio>
-                                <Radio value={"intermediate"}>Intermediate</Radio>
-                                <Radio value={"advanced"}>Advanced</Radio>
-                            </Radio.Group>
-                            <br/><br/><br/>
+                            <div className={skillStatusToClass}>
+                                <label>Skill</label><br/>
+                                <div className={"skill-collection"} style={{"marginTop": "10px"}}>
+                                    <div className={skillNameStatusToClass}>
+                                        <Input placeholder="Name"
+                                               prefix={<EditOutlined/>} value={skill}
+                                               onChange={skillOnChange}/>
+                                        {skillNameStatus !== "" &&
+                                            <span>{skillNameStatus}</span>}
+
+                                    </div>
+                                    <div className={skillYearStatusToClass}>
+                                        <Input placeholder="Experience in Years"
+                                               prefix={<EditOutlined/>} value={year}
+                                               onChange={yearOnChange}/>
+                                        {skillYearStatus !== "" &&
+                                            <span>{skillYearStatus}</span>}
+
+                                    </div>
+
+
+                                </div>
+                                {skillStatus === "Please fill in at least one skill" &&
+                                    <Alert message={skillStatus} type="error" showIcon/>}
+                                <Button
+                                    type="dashed"
+                                    style={{"marginTop": "10px"}}
+                                    onClick={handleAddSkill}
+                                    disabled={addBtnStatus}
+                                >
+                                    Add skill
+                                </Button>
+                                <br/><br/>
+                                <Table columns={columns} dataSource={state.skillList}
+                                       locale={{emptyText: 'Please add your skill using the above inputs field and button'}}/>
+
+                                <br/><br/>
+                            </div>
+
+                            <div className={proficiencyStatusToClass}>
+                                <label>Proficiency</label><br/>
+                                <Radio.Group onChange={proficiencyOnChange} value={proficiency}
+                                             className={"proficiency-collection " + proficiencyStatusToClass}
+                                             style={{"marginTop": "10px"}}>
+                                    <Radio value={"beginner"}>Beginner</Radio>
+                                    <Radio value={"intermediate"}>Intermediate</Radio>
+                                    <Radio value={"advanced"}>Advanced</Radio>
+                                </Radio.Group>
+                                <br/>
+                                {proficiencyStatus !== "" && <Alert message={proficiencyStatus} type="error" showIcon/>}
+                                <br/><br/>
+                            </div>
+
                         </div>
 
 
